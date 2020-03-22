b9b4028714cc9628ec89a103ae2959d6e29e55e7
afifahnurr <afifah.latifah@students.amikom.ac.id>
2020-03-22 22:23:57 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication3
+{
+    class Taxi
+    {
+        static void Main(string[] args)
+        {
+            Codingan Taxi = new Codingan();
+            Taxi.driverName = "Afifah";
+            Taxi.onDuty = true;
+            Taxi.NumPassenger = 10;
+
+            Taxi.TaxiInfo();
+            Console.WriteLine("\n");
+            Taxi.PickUpPassenger();
+            Taxi.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+
+
+}
+
+
+
